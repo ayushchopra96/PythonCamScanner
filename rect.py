dd5b707b9f2958c13fd4224e3d723c2aa01f49d5
ayushchopra96 <ayushchopra96@gmail.com>
2017-01-26 11:10:12 +0530

    v1

---
+++ rect.py
@@ -0,0 +1,17 @@
+import numpy as np
+
+def rectify(h):
+    h = h.reshape((4,2))
+    print h
+    hnew = np.zeros((4,2),dtype = np.float32)
+
+    add = h.sum(1)
+    print add
+    hnew[0] = h[np.argmin(add)]
+    hnew[2] = h[np.argmax(add)]
+
+    diff = np.diff(h,axis = 1)
+    hnew[1] = h[np.argmin(diff)]
+    hnew[3] = h[np.argmax(diff)]
+
+    return hnew
